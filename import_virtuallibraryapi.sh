b17b3818e33ab876fba0fee13c43ec9e335ab8df
daniel.oliveira <danielso2007@gmail.com>
2021-03-01 10:52:35 -0300

    feat: Novo import.

---
+++ import_virtuallibraryapi.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+RED='\033[0;31m'
+Black='\033[0;30m'
+Dark_Gray='\033[1;30m'
+Light_Red='\033[1;31m'
+Green='\033[0;32m'
+Light_Green='\033[1;32m'
+Brown_Orange='\033[0;33m'
+Yellow='\033[1;33m'
+Blue='\033[0;34m'
+Light_Blue='\033[1;34m'
+Purple='\033[0;35m'
+Light_Purple='\033[1;35m'
+Cyan='\033[0;36m'
+Light_Cyan='\033[1;36m'
+Light_Gray='\033[0;37m'
+White='\033[1;37m'
+NC='\033[0m' # No Color
+USERNAME="root"
+PASSWORD="112358"
+valid=true
+
+./mongoimport.sh virtuallibraryapi book.json book
+./mongoimport.sh virtuallibraryapi rating.json rating
